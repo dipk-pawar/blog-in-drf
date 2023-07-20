ca908105e8bc05cebbbbed5a5680a69ccb7ab00b
Dipak Pawar <pawardipak482@gmail.com>
2023-07-20 13:14:34 +0530

    added JWT authentications

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import BlogPost
 from .serializers import BlogPostSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
 class BlogPostListCreateView(generics.ListCreateAPIView):
-    queryset = BlogPost.objects.all()
+    permission_classes = [IsAuthenticated]
     serializer_class = BlogPostSerializer
+    queryset = BlogPost.objects.all()
 
 
 class BlogPostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
     "apps.accounts",
     "apps.posts",
 ]
@@ -124,3 +127,16 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "accounts.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+]
+SIMPLE_JWT = {"ACCESS_TOKEN_LIFETIME": timedelta(minutes=50)}
+PASSWORD_RESET_TIMEOUT = 900  # time in second means 900 sec = 15 Min
