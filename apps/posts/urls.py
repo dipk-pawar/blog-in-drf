24755d247a2d3df568da8cc80e312fbcc3ee931d
Dipak Pawar <pawardipak482@gmail.com>
2023-07-20 18:49:52 +0530

    added the comment section

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
-from .views import BlogPostListCreateView, BlogPostRetrieveUpdateDestroyView
+from .views import (
+    BlogPostListCreateView,
+    BlogPostRetrieveUpdateDestroyView,
+    AllBlogPostsAPIView,
+    CommentsAPIView,
+)
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"comments", CommentsAPIView, basename="comments")
 
 urlpatterns = [
+    path("all-posts/", AllBlogPostsAPIView.as_view(), name="all-blogpost-list"),
     path("", BlogPostListCreateView.as_view(), name="blogpost-list-create"),
     path(
         "<int:pk>/",
         BlogPostRetrieveUpdateDestroyView.as_view(),
         name="blogpost-detail",
     ),
-]
+] + router.urls
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
 from rest_framework import generics
-from .models import BlogPost
-from .serializers import BlogPostSerializer
-from rest_framework.permissions import IsAuthenticated
+from .models import BlogPost, Comment
+from .serializers import BlogPostSerializer, CommentSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.viewsets import ModelViewSet
 
 
 # Create your views here.
+class AllBlogPostsAPIView(generics.ListAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = BlogPostSerializer
+    queryset = BlogPost.objects.all()
+
+
 class BlogPostListCreateView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = BlogPostSerializer
@@ -22,3 +28,12 @@ class BlogPostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         user = self.request.user
         return BlogPost.objects.filter(author=user)
+
+
+class CommentsAPIView(ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Comment.objects.filter(author=user)
