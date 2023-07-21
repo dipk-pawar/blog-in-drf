9d911bc7693e3ac91e167db646944a17fb1249d0
Dipak Pawar <pawardipak482@gmail.com>
2023-07-21 14:57:29 +0530

    changes in create user testcases

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,84 +1,64 @@
-from django.test import TestCase
 from django.urls import reverse
-from .models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-# Create your tests here.
-# Tests that a user can be created with valid input data
-class CreateUserTestCase(TestCase):
-    def test_create_user_valid_input(self):
-        data = {
-            "email": "test@test.com",
-            "first_name": "Test",
-            "last_name": "User",
-            "password": "Test1234",
+class UserCreateAPITest(APITestCase):
+    def setUp(self):
+        self.url = reverse("register")
+
+        # Test data for creating a new user
+        self.valid_user_data = {
+            "email": "test@example.com",
+            "first_name": "Dipak",
+            "last_name": "Pawar",
+            "password": "testpassword",
         }
-        url = reverse("register")
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 201)
 
-    # Tests that a user can be created with a password containing special characters
-    def test_create_user_special_characters_password(self):
-        data = {
-            "email": "test@test.com",
-            "first_name": "Test",
-            "last_name": "User",
-            "password": "Test@1234",
-        }
-        url = reverse("register")
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 201)
+    def test_create_user_success(self):
+        response = self.client.post(self.url, data=self.valid_user_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    # Tests that a user can be created with a password containing numbers
-    def test_create_user_numbers_password(self):
-        data = {
-            "email": "test@test.com",
-            "first_name": "Test",
-            "last_name": "User",
-            "password": "Test1234",
-        }
-        url = reverse("register")
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 201)
-
-    # Tests that a user can be created with a password containing uppercase and lowercase letters
-    def test_create_user_uppercase_lowercase_password(self):
-        data = {
-            "email": "test@test.com",
-            "first_name": "Test",
-            "last_name": "User",
-            "password": "Test1234",
-        }
-        url = reverse("register")
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 201)
-
-    # Tests that a user cannot be created with an existing email
-    def test_create_user_existing_email(self):
-        User.objects.create_user(
-            email="test@test.com",
-            first_name="Existing",
-            last_name="User",
-            password="Test1234",
+        # Check if the user was created with the correct data
+        self.assertTrue(
+            User.objects.filter(email=self.valid_user_data["email"]).exists()
         )
-        data = {
-            "email": "test@test.com",
-            "first_name": "Test",
-            "last_name": "User",
-            "password": "Test1234",
-        }
-        url = reverse("register")
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 400)
+        user = User.objects.get(email=self.valid_user_data["email"])
+        self.assertEqual(user.first_name, self.valid_user_data["first_name"])
+        self.assertEqual(user.last_name, self.valid_user_data["last_name"])
+        self.assertTrue(user.check_password(self.valid_user_data["password"]))
 
-    # Tests that a user cannot be created with an invalid email
-    def test_create_user_invalid_email(self):
-        data = {
-            "email": "invalid_email",
-            "first_name": "Test",
-            "last_name": "User",
-            "password": "Test1234",
+        # Check if sensitive data like "password", "is_active", "is_staff" are not returned in the response
+        self.assertNotIn("password", response.data)
+        self.assertNotIn("is_active", response.data)
+        self.assertNotIn("is_staff", response.data)
+
+    def test_create_user_missing_email_password(self):
+        # Test case for missing "email" and "password" fields
+        invalid_data = {
+            "first_name": "Dipak",
+            "last_name": "Pawar",
         }
-        url = reverse("register")
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 400)
+        response = self.client.post(self.url, data=invalid_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # Check if the response contains the error message for missing "email" and "password"
+        self.assertIn("email", response.data)
+        self.assertIn("password", response.data)
+
+    def test_create_user_invalid_email(self):
+        # Test case for an invalid email address
+        invalid_data = {
+            "email": "invalid_email",
+            "first_name": "Dipak",
+            "last_name": "Pawar",
+            "password": "testpassword",
+        }
+        response = self.client.post(self.url, data=invalid_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # Check if the response contains the error message for an invalid email address
+        self.assertIn("email", response.data)
