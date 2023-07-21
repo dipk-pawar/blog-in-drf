1cc835488ca80dd1b4101ceea8da6c650291f267
Dipak Pawar <pawardipak482@gmail.com>
2023-07-21 15:29:54 +0530

    added the postman collection

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -1,7 +1,6 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from django.contrib.auth import get_user_model
 from .models import User
 
 
